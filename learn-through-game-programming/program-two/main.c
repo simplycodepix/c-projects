fba1624f25da8342852f416ed106db39d3430919
vladislav.godunov <codeforeveryone.rj@gmail.com>
2018-02-28 05:30:51 +0200

    Learn Through Game Programming - Word Jumble

---
+++ learn-through-game-programming/program-two/main.c
@@ -0,0 +1,15 @@
+/*
+* Improve the Word Jumble game by adding a scoring system.
+* Make the point value for a word based on its length.
+* Deduct points if the player asks for a hint.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("works");
+    return 0;
+
+}
