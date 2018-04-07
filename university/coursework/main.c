5688581d2dd949f7e07a977e18c85204039b4204
vladislav.godunov <codeforeveryone.rj@gmail.com>
2018-04-07 10:38:29 +0300

    University - Lab11 Initial Commit

--- university/coursework/main.c
+++ university/coursework/main.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <math.h>
+
+#define T0 100
+#define ALPHA 0.05
+
+double stocksFormula(double viscosity, float speed, float radius);
+double calculateTemperature();
+double calculateViscosity(double temperature);
+
+struct DATASTRUCTURE
+{
+
+    int radius;
+    int delta;
+    
+
+} data[3]
 
 int main(void)
 {
