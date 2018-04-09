7eb800b3ae5ea2c6d30bbf6e607bc4528d2c4b8c
vladislav.godunov <codeforeveryone.rj@gmail.com>
2018-04-09 17:41:22 +0300

    University - Lab 11. isMatrixSymmetrical()

--- university/lab11/main.c
+++ university/lab11/main.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ARRAY_SIZE 3
+
 float fillArray(float* array, int size);
 float calculateAverage(float* array, int size);
 void printArray(float* array, int size);
 void findMaxElementAndItsPosition(float* array, int size, float* max, int* position);
+void isMatrixSymmetrical(float array[ARRAY_SIZE][ARRAY_SIZE], int size);
 
 int main(void)
 {
 
-    float *arrayOfNumbers, average;
+    float *arrayOfNumbers, average, matrix[ARRAY_SIZE][ARRAY_SIZE] = {1, -2, 5, -2, 2, 4, 5, 4, 3};
+    // = {1, -2, 5, -2, 2, 4, 5, 4, 3}
     int sizeOfArray;
+
+    printf("This is your Matrix\n");
+    for(int i = 0; i < ARRAY_SIZE; i++)
+    {
+        for(int j = 0; j < ARRAY_SIZE; j++)
+            printf("%4.2f ", matrix[i][j]);
+        printf("\n");
+    }
+
+    isMatrixSymmetrical(matrix, ARRAY_SIZE);
 
     printf("Enter size of an Array: ");
     fflush(stdin);
@@ -51,9 +65,7 @@ float fillArray(float* array, int size)
 void printArray(float* array, int size)
 {
     for(int i = 0; i < size; i++)
-    {
         printf("%4.2f ", array[i]);     
-    }
     printf("\n");
 }
 
@@ -61,9 +73,7 @@ float calculateAverage(float* array, int size)
 {
     float sum = 0;
     for(int i = 0; i < size; i++)
-    {
         sum += *array++;
-    }
     return (sum / size);
 }
 
@@ -80,4 +90,17 @@ void findMaxElementAndItsPosition(float* array, int size, float* max, int* posit
             *position = i;
         }
     }
+}
+
+void isMatrixSymmetrical(float array[ARRAY_SIZE][ARRAY_SIZE], int size)
+{
+    int counter = 0;
+
+    for(int i = 0; i < size; i++)
+        for(int j = 0; j < size; j++)
+            if(array[i][j] == array[j][i])
+                counter++;
+    
+    if(counter == (size * size))
+        printf("Your Matrix is Symmetrical\n");
 }
